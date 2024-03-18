27bfc3f05cfbd64ebbd8979ce33bc3002bd8b718
minnug <parkminnug@gmail.com>
2024-03-18 15:20:11 +0900

    🎉 Javascript 프로젝트 시작: 직원관리 서비스

---
+++ src/routes/Home.js
@@ -0,0 +1,7 @@
+import { Component } from "../core/heropy"
+
+export default class Home extends Component {
+  render() {
+    this.el.innerHTML = '메인페이지입니다.111'
+  }
+}
---
+++ src/routes/index.js
@@ -0,0 +1,6 @@
+import { Component, createRouter } from "../core/heropy"
+import Home from "./Home"
+
+export default createRouter([
+  { path: '#/', component: Home }
+])
