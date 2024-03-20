ecd185fbd069cde70b11241d6a1ca2411f57e3bb
minnug <parkminnug@gmail.com>
2024-03-20 15:45:37 +0900

    ✨ 캐릭터 추가 페이지 - 이미지 업로드 구현

--- src/routes/NewProfile.js
+++ src/routes/NewProfile.js
@@ -6,8 +6,14 @@ export default class View extends Component {
     this.el.innerHTML = /* html */ `
       <div class="container">
         <h1 class="title">NEW&nbsp;<span class="logo--black"></span>&nbsp;CHARACTER</h1>
-        <div class="form">
-          <div class="img"></div>
+        <form class="form">
+          <div class="upload-file">
+            <div id="viewImg" class="img-view"></div>
+            <label for="file">
+              <span class="btn">Click to Upload Image</span>
+              <input type="file" id="file" accept="image/*" class="input-file">
+            </label>
+          </div>
           <div class="input__field">
             <label>
               Name
@@ -27,11 +33,21 @@ export default class View extends Component {
             </label>
           </div>
           <div class="btn-group">
-            <button type="submit" class="btn">Create</button> 
+            <button type="submit" class="btn">Upload</button> 
             <button type="submit" class="btn btn--red">Cancle</button> 
           </div> 
-        </div>
+        </form>
       </div>
     `
-  }
-}
+    // 이미지 업로드
+    const fileInput = this.el.querySelector('.input-file');
+    fileInput.addEventListener('change', () => {
+      const fileReader = new FileReader();
+      fileReader.addEventListener('load', () => {
+        const uploadImg = fileReader.result;
+        this.el.querySelector('.img-view').style.backgroundImage = `url(${uploadImg})`;
+      })
+      fileReader.readAsDataURL(fileInput.files[0]);
+    })
+  };
+};
